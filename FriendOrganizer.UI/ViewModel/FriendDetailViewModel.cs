fb3e2297526752a1b36d0b250110389b249e8def
Mad-Mange <madheli@hotmail.com>
2022-01-10 06:23:11 +0100

    First commit

---
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -0,0 +1,67 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using FriendOrganizer.UI.Wrapper;
+using Prism.Commands;
+using Prism.Events;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
+    {
+        private IFriendDataService _dataService;
+        private IEventAggregator _eventAggregator;
+        private FriendWrapper _friend;
+
+        public FriendDetailViewModel(IFriendDataService dataService, IEventAggregator eventAggregator)
+        {
+            _dataService = dataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                .Subscribe(OnOpenFriendDetailView);
+            SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
+        }
+
+        public async Task LoadAsync(int friendId)
+        {
+            var friend = await _dataService.GetByIdAsync(friendId);
+            Friend = new FriendWrapper(friend);
+        }
+
+        public FriendWrapper Friend
+        {
+            get { return _friend; }
+            set { _friend = value; OnPropertyChanged(); }
+        }
+
+        public ICommand SaveCommand { get; }
+
+        private async void OnSaveExecute()
+        {
+            await _dataService.SaveAsync(Friend.Model);
+            _eventAggregator.GetEvent<AfterFriendSaveEvent>().Publish(
+                new AfterFriendSaveEventArgs
+                {
+                    Id = Friend.Id,
+                    DisplayMember = $"{Friend.FirstName} {Friend.LastName}"
+                });
+        }
+
+        private bool OnSaveCanExecute()
+        {
+            // Check if friend is valid
+            return true;
+        }
+
+        private async void OnOpenFriendDetailView(int friendId)
+        {
+            await LoadAsync(friendId);
+        }
+
+
+
+    }
+}
